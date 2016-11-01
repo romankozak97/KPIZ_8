71d20cbeb2a1cd0592c50ff1b440ebce9d85f2cd
Administrator <romankozak97.ua@gmail.com>
2016-11-01 17:09:51 +0200

    Added primitive JUnit tests

---
+++ src/com/kpiz/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.kpiz;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Administrator on 11/1/2016.
+ */
+public class ApplicationTest extends TestCase {
+    public void testInitUI() throws Exception {
+        assertTrue(1>1);
+    }
+}
---
+++ src/com/kpiz/SurfaceTest.java
@@ -0,0 +1,17 @@
+package com.kpiz;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Administrator on 11/1/2016.
+ */
+public class SurfaceTest extends TestCase {
+    public void testconvertXCoords() throws Exception {
+        Surface obj = new Surface();
+        assertFalse(1>1);
+    }
+
+    public void testconvertYCoords() throws Exception {
+        assertFalse(1>1);
+    }
+}
